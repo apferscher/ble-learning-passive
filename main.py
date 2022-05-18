4e9164c00bd05d91334616fb643910eace7983b5
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-05-18 11:34:24 +0200

    add model comparison

--- main.py
+++ main.py
@@ -66,13 +66,13 @@ def compare_learned_models(model_1, model_2, num_tests, min_test_len=3, max_test
     return diff / num_tests
 
 
-def compare_learned_models_state_coverage(model_1, model_2, num_tests):
+def compare_learned_models_with_state_coverage(model_1, model_2, num_tests):
     diff = 0
-    inputs = model_1.get_input_alphabet()
+    inputs = model_2.get_input_alphabet()
 
-    walks_per_state = int(num_tests / model_1.size)
+    walks_per_state = int(num_tests / model_2.size)
     eq_oracle = StatePrefixEqOracle(inputs, sul=None, walks_per_state=walks_per_state, walk_len=10)
-    test_cases = [tc[0] for tc in generate_test_cases(model_1, eq_oracle)]
+    test_cases = [tc[0] for tc in generate_test_cases(model_2, eq_oracle)]
 
     for test_case in test_cases:
         o_1 = model_1.compute_output_seq(model_1.initial_state, test_case)
@@ -102,17 +102,19 @@ for model_name, model in bluetooth_models:
     learning_queries = data['queries_learning']
     eq_oracle_queries = data['queries_eq_oracle']
 
-    print(f'Number of queries required by L*: {learning_queries}')
-
     # data = data_from_l_star_E_set(l_star_model, e_set, include_extended_s_set=True)
     # data = data_from_computed_e_set(l_star_model, include_extended_s_set=True)
     data = minimized_char_set_data(l_star_model, e_set, include_extended_s_set=True)
     data = generate_random_data(model, num_sequences=learning_queries - 100, min_sequence_len=10, max_sequence_len=20)
     # print(learning_queries - len(data))
 
-    rpni_model = run_RPNI(data, automaton_type='mealy', input_completeness='sink_state', print_info=True)
+    rpni_model = run_RPNI(data, automaton_type='mealy', input_completeness='sink_state', print_info=False)
 
     print(f'Experiment: {model_name}')
+    print(f'L* learned {l_star_model.size} state model.')
+    print(f'RPNI Learned {rpni_model.size} state model.')
+    print(f'Number of queries required by L*  : {learning_queries}')
+    print(f'Number of samples provided to RPNI: {len(data)}')
 
     if set(rpni_model.get_input_alphabet()) != set(l_star_model.get_input_alphabet()):
         print('Learned models do not have the same input alphabets => RPNI model is not input complete.')
@@ -120,8 +122,8 @@ for model_name, model in bluetooth_models:
 
     cex = compare_automata(rpni_model, l_star_model)
     if cex:
-        model_diff = compare_learned_models(l_star_model, rpni_model, num_tests=10000)
-        # model_diff = compare_learned_models_state_coverage(l_star_model, rpni_model, num_tests=10000)
+        # model_diff = compare_learned_models(l_star_model, rpni_model, num_tests=10000)
+        model_diff = compare_learned_models_with_state_coverage(l_star_model, rpni_model, num_tests=10000)
         print('Counterexample found between models learned by RPNI and L*.')
         print(f'Models display different bahaviour for {round(model_diff * 100, 2)}% of test cases.')
     else:
@@ -129,4 +131,4 @@ for model_name, model in bluetooth_models:
         if rpni_model.size != l_star_model.size:
             print(f'    Models do have different size.\n    RPNI {rpni_model.size} vs. L* {l_star_model.size}')
 
-    print('-----------------------------------------------')
+    print('----------------------------------------------------------------')
