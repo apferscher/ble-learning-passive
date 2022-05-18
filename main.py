2b6d1d769638ccab1c0a2c2edc3e813893b6c0d4
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-05-18 12:02:07 +0200

    add test case generation on top of the script

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def generate_random_data(model, num_sequences, min_sequence_len, max_sequence_le
     return data
 
 
-def compare_learned_models(model_1, model_2, num_tests, min_test_len=3, max_test_len=16):
+def compare_learned_models_random_word(model_1, model_2, num_tests, min_test_len=3, max_test_len=16):
     diff = 0
     inputs = model_1.get_input_alphabet()
     for _ in range(num_tests):
@@ -66,13 +66,8 @@ def compare_learned_models(model_1, model_2, num_tests, min_test_len=3, max_test
     return diff / num_tests
 
 
-def compare_learned_models_with_state_coverage(model_1, model_2, num_tests):
+def compare_learned_models(model_1, model_2, test_cases):
     diff = 0
-    inputs = model_2.get_input_alphabet()
-
-    walks_per_state = int(num_tests / model_2.size)
-    eq_oracle = StatePrefixEqOracle(inputs, sul=None, walks_per_state=walks_per_state, walk_len=10)
-    test_cases = [tc[0] for tc in generate_test_cases(model_2, eq_oracle)]
 
     for test_case in test_cases:
         o_1 = model_1.compute_output_seq(model_1.initial_state, test_case)
@@ -84,12 +79,25 @@ def compare_learned_models_with_state_coverage(model_1, model_2, num_tests):
     return diff / len(test_cases)
 
 
+def create_test_cases(experiment_list, num_test_cases):
+    test_cases = dict()
+    for model_name, model in experiment_list:
+        inputs = model.get_input_alphabet()
+        walks_per_state = int(num_test_cases / model.size)
+        eq_oracle = StatePrefixEqOracle(inputs, sul=None, walks_per_state=walks_per_state, walk_len=10)
+        test_cases[model_name] = [tc[0] for tc in generate_test_cases(model, eq_oracle)]
+
+    return test_cases
+
+
 bluetooth_models = []
 
 for dot_file in listdir('./automata'):
     model_name = dot_file[:-4]
     model = load_automaton_from_file(f'./automata/{dot_file}', automaton_type='mealy')
     bluetooth_models.append((model_name, model))
+
+test_cases = create_test_cases(bluetooth_models, 10000)
 
 for model_name, model in bluetooth_models:
     sul = MealySUL(model)
@@ -123,7 +131,7 @@ for model_name, model in bluetooth_models:
     cex = compare_automata(rpni_model, l_star_model)
     if cex:
         # model_diff = compare_learned_models(l_star_model, rpni_model, num_tests=10000)
-        model_diff = compare_learned_models_with_state_coverage(l_star_model, rpni_model, num_tests=10000)
+        model_diff = compare_learned_models(l_star_model, rpni_model, test_cases[model_name])
         print('Counterexample found between models learned by RPNI and L*.')
         print(f'Models display different bahaviour for {round(model_diff * 100, 2)}% of test cases.')
     else:
