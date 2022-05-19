73f006df8f18e190228ea365a2bbe30850e73bc7
Andrea Pferscher <andrea.pferscher@ist.tugraz.at>
2022-05-19 19:45:06 +0200

    experiment setup

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aalpy.SULs import MealySUL
 from aalpy.learning_algs import run_Lstar, run_RPNI
 from aalpy.oracles import RandomWordEqOracle, StatePrefixEqOracle
 from aalpy.utils import load_automaton_from_file, compare_automata, generate_test_cases
-
+from math import ceil
 
 def data_from_l_star_E_set(hypothesis, e_set, include_extended_s_set=True):
     data = []
@@ -107,36 +107,62 @@ for model_name, model in bluetooth_models:
     l_star_model, data = run_Lstar(alphabet, sul, eq_oracle, 'mealy', print_level=0, return_data=True)
 
     e_set = data['characterization set']
-    learning_queries = data['queries_learning']
+    output_queries = data['queries_learning']
     eq_oracle_queries = data['queries_eq_oracle']
 
-    # data = data_from_l_star_E_set(l_star_model, e_set, include_extended_s_set=True)
-    # data = data_from_computed_e_set(l_star_model, include_extended_s_set=True)
-    data = minimized_char_set_data(l_star_model, e_set, include_extended_s_set=True)
-    data = generate_random_data(model, num_sequences=learning_queries - 100, min_sequence_len=10, max_sequence_len=20)
+    learning_queries = output_queries + eq_oracle_queries
+    learning_steps = int(ceil((output_queries + eq_oracle_queries) / (data['steps_learning'] + data['steps_eq_oracle'])))
+    max_sequence_length = learning_steps * 2
+
+    data_l_star = data_from_computed_e_set(l_star_model, include_extended_s_set=True)
+
+    data_random_l_star_length = generate_random_data(model, num_sequences=learning_queries, min_sequence_len=1, max_sequence_len=max_sequence_length)
+
+    data_random_large_set = generate_random_data(model, num_sequences=(learning_queries * 2), min_sequence_len=1, max_sequence_len=max_sequence_length)
+
+    data_random_long_traces = generate_random_data(model, num_sequences=ceil(learning_queries / 2), min_sequence_len=l_star_model.size, max_sequence_len=(l_star_model.size * 2))
+
+    data_minimized_char_set = minimized_char_set_data(l_star_model, e_set, include_extended_s_set=True)
+    
     # print(learning_queries - len(data))
 
-    rpni_model = run_RPNI(data, automaton_type='mealy', input_completeness='sink_state', print_info=False)
+    rnpi_models = {}
+
+    rnpi_models['rpni_model_random_l_star_length'] = (run_RPNI(data_random_l_star_length, automaton_type='mealy', input_completeness='sink_state', print_info=False), data_random_l_star_length)
+
+    rnpi_models['rpni_model_andom_large_set'] = (run_RPNI(data_random_large_set, automaton_type='mealy', input_completeness='sink_state', print_info=False), data_random_large_set)
+
+    rnpi_models['rpni_model_random_long_traces'] = (run_RPNI(data_random_long_traces, automaton_type='mealy', input_completeness='sink_state', print_info=False), data_random_long_traces)
+
+    rnpi_models['rpni_model_l_star'] = (run_RPNI(data_l_star, automaton_type='mealy', input_completeness='sink_state', print_info=False), data_l_star)
+
+    rnpi_models['rpni_model_minimized_char_set'] = (run_RPNI(data_minimized_char_set, automaton_type='mealy', input_completeness='sink_state', print_info=False), data_minimized_char_set)
+
 
     print(f'Experiment: {model_name}')
     print(f'L* learned {l_star_model.size} state model.')
-    print(f'RPNI Learned {rpni_model.size} state model.')
     print(f'Number of queries required by L*  : {learning_queries}')
-    print(f'Number of samples provided to RPNI: {len(data)}')
 
-    if set(rpni_model.get_input_alphabet()) != set(l_star_model.get_input_alphabet()):
-        print('Learned models do not have the same input alphabets => RPNI model is not input complete.')
-        continue
+    for k in rnpi_models.keys():
+        rpni_model = rnpi_models[k][0]
+        data = rnpi_models[k][1]
+        print('-' * 5 + f' {k} ' + '-' * 5)
+        print(f'RPNI Learned {rpni_model.size} state model.')
+        print(f'Number of samples provided to RPNI: {len(data)}')
 
-    cex = compare_automata(rpni_model, l_star_model)
-    if cex:
-        # model_diff = compare_learned_models(l_star_model, rpni_model, num_tests=10000)
-        model_diff = compare_learned_models(l_star_model, rpni_model, test_cases[model_name])
-        print('Counterexample found between models learned by RPNI and L*.')
-        print(f'Models display different bahaviour for {round(model_diff * 100, 2)}% of test cases.')
-    else:
-        print('RPNI and L* learned same models.')
-        if rpni_model.size != l_star_model.size:
-            print(f'    Models do have different size.\n    RPNI {rpni_model.size} vs. L* {l_star_model.size}')
+        if set(rpni_model.get_input_alphabet()) != set(l_star_model.get_input_alphabet()):
+            print('Learned models do not have the same input alphabets => RPNI model is not input complete.')
+            continue
+
+        cex = compare_automata(rpni_model, l_star_model)
+        if cex:
+            # model_diff = compare_learned_models(l_star_model, rpni_model, num_tests=10000)
+            model_diff = compare_learned_models(l_star_model, rpni_model, test_cases[model_name])
+            print('Counterexample found between models learned by RPNI and L*.')
+            print(f'Models display different bahaviour for {round(model_diff * 100, 2)}% of test cases.')
+        else:
+            print('RPNI and L* learned same models.')
+            if rpni_model.size != l_star_model.size:
+                print(f'    Models do have different size.\n    RPNI {rpni_model.size} vs. L* {l_star_model.size}')
 
     print('----------------------------------------------------------------')
