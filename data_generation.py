7497d32ef6c9c29c06f8f3ab0c8b9aa61af4cc00
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-06-17 10:13:45 +0200

    update print and add mealy moore comp

--- data_generation.py
+++ data_generation.py
@@ -1,11 +1,8 @@
 import random
 
-from math import ceil
-
 from aalpy.SULs import MealySUL
 from aalpy.base.SUL import CacheSUL
 from aalpy.learning_algs import run_Lstar
-from aalpy.oracles import RandomWordEqOracle
 from aalpy.utils.HelperFunctions import all_prefixes
 
 
@@ -38,7 +35,7 @@ def data_from_l_star_E_set(hypothesis, e_set, include_extended_s_set=True, prefi
 
     if verbose:
         print(f'Number of samples provided to RPNI: {len(data_set_tmp)}')
-        print(f'Average length of samples provided to RPNI: {average_length}')
+        print(f'Average length of samples provided to RPNI: {round(average_length, 2)}')
 
     for seq in list(data_set):
         output = hypothesis.compute_output_seq(hypothesis.initial_state, seq)[-1]
@@ -64,7 +61,7 @@ def minimized_char_set_data(hypothesis, include_extended_s_set=True, prefix_clos
 
     if verbose:
         print(f'Number of samples provided to RPNI: {len(input_sequences)}')
-        print(f'Average length of samples provided to RPNI: {average_length}')
+        print(f'Average length of samples provided to RPNI: {round(average_length, 2)}')
 
     if prefix_closed:
         prefix_closed_seq = set()
@@ -91,7 +88,7 @@ def generate_random_data(model, num_sequences, min_sequence_len, max_sequence_le
 
     if verbose:
         print(f'Number of samples provided to RPNI: {len(random_sequences)}')
-        print(f'Average length of samples provided to RPNI: {average_length}')
+        print(f'Average length of samples provided to RPNI: {round(average_length, 2)}')
 
     if prefix_closed:
         prefix_closed_seq = set()
--- model_comparison.py
+++ model_comparison.py
@@ -1,7 +1,6 @@
-import random
-
-from aalpy.oracles import StatePrefixEqOracle, RandomWalkEqOracle, RandomWordEqOracle
+from aalpy.oracles import StatePrefixEqOracle, RandomWordEqOracle
 from aalpy.utils import generate_test_cases
+
 
 def compare_learned_models(model_1, model_2, test_cases):
     diff = 0
@@ -29,3 +28,25 @@ def create_test_cases(experiment_list, num_test_cases, method):
         test_cases[model_name] = [tc[0] for tc in generate_test_cases(model, eq_oracle)]
 
     return test_cases
+
+
+def compare_mealy_and_moore_learning():
+    from aalpy.learning_algs import run_RPNI
+    from aalpy.utils import load_automaton_from_file
+
+    from data_generation import generate_random_data
+
+    model = load_automaton_from_file('automata/CYBLE-416045-02.dot', automaton_type='mealy')
+    data = generate_random_data(model, num_sequences=10000, min_sequence_len=3, max_sequence_len=8)
+
+    model_mealy = run_RPNI(data, automaton_type='mealy', input_completeness='sink_state')
+
+    model_moore = run_RPNI(data, automaton_type='moore', input_completeness='sink_state')
+
+    tc_random = create_test_cases([('ex1', model)], 10000, 'random')['ex1']
+    # tc_coverage = create_test_cases([('ex1', model)], 10000, 'coverage')
+
+    res_random1 = compare_learned_models(model, model_mealy, tc_random)
+    res_random2 = compare_learned_models(model, model_moore, tc_random)
+
+    print(f'Mealy {round((1 - res_random1) * 100)}, Moore {round((1 - res_random2) * 100)}')
